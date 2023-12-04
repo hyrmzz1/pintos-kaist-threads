347b0842e2f7ee39fb21364a8a24b67b27d5f16c
hyrmzz1 <hlhlstar@naver.com>
2023-12-04 09:49:08 +0900

    fixed_point.h 전방 선언

--- include/threads/fixed_point.h
+++ include/threads/fixed_point.h
@@ -5,6 +5,18 @@
 // x and y denote fixed_point numbers in 17.14 format
 // n is an integer
 
+int int_to_fp(int n);
+int fp_to_int_round(int x);
+int fp_to_int(int x);
+int add_fp(int x, int y);
+int add_mixed(int x, int n);
+int sub_fp(int x, int y);
+int sub_mixed(int x, int n);
+int mult_fp(int x, int y);
+int mult_mixed(int x, int y);
+int div_fp(int x, int y);
+int div_mixed(int x, int n);
+
 int int_to_fp(int n){
     return n * F;
 }
